14417391f70b70983ee0122b9be41c4162979585
richardkmi <83045076+richardkmi@users.noreply.github.com>
2024-01-10 17:06:00 -0500

    Update mtcars.csv and streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,12 @@
 import streamlit as st
+import pandas as pd
+from pivottablejs import pivot_ui
 
-st.write("Hello, World!")
+df = pd.read_csv("mtcars.csv")
+
+# Convert the DataFrame to a JSON string
+json_data = df.to_json(orient="records")
+
+# Render the PivotTable using pivottablejs
+pivot_ui(json_data)
+
